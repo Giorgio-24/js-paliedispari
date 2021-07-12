099f613e5374ac62db31e6bd8e273a58682341bc
Giorgio Andrei <cipuandrei@libero.it>
2021-07-12 17:07:04 +0200

    even-or-odd-game-done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var userWord = prompt('Inserisci una parola: ');
+var userWord = prompt('Inserisci una parola:');
 console.log(userWord);
 
 function checkPalindrom(userWord) {
@@ -11,3 +11,41 @@ CI RESTITUIRA' TRUE, IN CASO CONTRARIO IL RISULTATO SAREBBE FALSE.*/
 
 console.log(checkPalindrom(userWord));
 
+
+var sum = 0;
+var winOrLoose;
+var userNumber = parseInt(prompt('Inserisci un numero da 1 a 5:'));
+
+
+
+if ((userNumber >= 1 && userNumber <= 5)) {
+    var userChioce = prompt('Scegli tra pari e dispari:');
+} else {
+    alert('ATTENZIONE!! NON HAI INSERITO UN NUMERO VALIDO.')
+}
+
+function cpuRandom() {
+    cpuNumber = Math.floor(Math.random() * 5) + 1
+    return cpuNumber;
+}
+
+sum = userNumber + cpuRandom();
+console.log(sum);
+
+function evenOrOdd() {
+    var bool = false;
+    if ((sum % 2 === 0 && userChioce == 'pari') || (sum % 2 !== 0 && userChioce == 'dispari')) {
+        bool = true;
+    }
+    return bool;
+}
+
+var y = evenOrOdd();
+
+if (y === true) {
+    winOrLoose = 'CONGRATULAZIONI! HAI VINTO:';
+} else {
+    winOrLoose = 'OH NO! HAI PERSO:';
+}
+
+var result = console.log(winOrLoose);
